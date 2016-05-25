44954f7afbe5f359c5c9f5558758400a68bd3ba3
Emily Chen <e3chen@ucsd.edu>
2016-05-24 20:04:53 -0700

    adding all files

---
+++ app/src/androidTest/java/com/emilychen/espressotest/tests/MainActivitytest.java
@@ -0,0 +1,47 @@
+package com.emilychen.espressotest.tests;
+
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.emilychen.espressotest.MainActivity;
+import com.emilychen.espressotest.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+/**
+ * Created by emilychen on 5/24/16.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivitytest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testText() {
+        String expectedTest = "Hello World!";
+        onView(withId(R.id.textView)).check(matches(withText(expectedTest)));
+    }
+
+    @Test
+    public void sayHello() {
+        onView(withId(R.id.editText)).perform(typeText("emily"), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.button)).perform(click());
+        String expectedText = "emily";
+        onView(withId(R.id.textView)).check(matches(withText(expectedText)));
+    }
+}
